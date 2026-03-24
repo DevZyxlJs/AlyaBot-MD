7a9ed34bca216bc9676b1cb2b3d19213a51b7273
ɪ'ᴍ ᴅɪᴇɢᴏ ᜊ•ᴗ•ᜊ <sarmientodieg@gmail.com>
2026-03-24 10:11:59 -0500

    Actualizar chatgpt.js

--- commands/ai/chatgpt.js
+++ commands/ai/chatgpt.js
@@ -1,34 +1,38 @@
-import { ai } from 'cloudst'
+import fetch from 'node-fetch';
 
 export default {
   command: ['ia', 'chatgpt'],
   category: 'ai',
   run: async (client, m, args, command) => {
-    const text = args.join(' ').trim()
+
+    const text = args.join(' ').toLowerCase()
 
     if (!text) {
       return m.reply(`✎ Escriba una *petición* para que *ChatGPT* le responda.`)
     }
 
+    const apiUrl = `${api.url}/ai/chatgpt?text=${encodeURIComponent(text)}&key=${api.key}`
+
     try {
-      const txc = `✎ *ChatGPT* está procesando tu respuesta...`
+     const txc = `✎ *ChatGPT* está procesando tu respuesta...`;
       const { key } = await client.sendMessage(
         m.chat,
         { text: txc },
         { quoted: m },
       )
 
-      const res = await ai.chat(text)
+      const res = await fetch(apiUrl)
+      const json = await res.json()
 
-      if (!res || !res.result) {
+      if (!json || !json.result) {
         return client.reply(m.chat, '✎ No se pudo obtener una *respuesta* válida')
       }
 
-      const response = `${res.result}`.trim()
+      const response = `${json.result}`.trim()
       await client.sendMessage(m.chat, { text: response, edit: key })
     } catch (error) {
       console.error(error)
-      await m.reply('✎ Hubo un problema al procesar tu petición.')
+      await m.reply(msgglobal)
     }
   },
-}
+};
