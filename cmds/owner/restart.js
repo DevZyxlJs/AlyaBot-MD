f910cfb129be3f80bfc8d25230c0b2f76f0b9dbc
DevZyxlJs <sarmientodieg@gmail.com>
2026-06-03 22:21:14 -0500

    Repo limpio sin secretos

---
+++ cmds/owner/restart.js
@@ -0,0 +1,11 @@
+export default {
+  command: ['restart'],
+  category: 'mod',
+  isOwner: true,
+  run: async ({ msg, sock }) => {
+    await sock.reply(msg.chat, `✎ Reiniciando el Socket...\n> *Espere un momento...*`, msg)
+    setTimeout(() => {
+      process.exit(0)
+    }, 3000)
+  },
+};
